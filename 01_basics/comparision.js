4e9cf4f221a54f8c20c8f6ff94ca1b28d2c4ca86
RiyaaBhandari <127602057+RiyaaBhandari@users.noreply.github.com>
2024-03-03 11:16:59 +0000

    comparision in js

---
+++ 01_basics/comparision.js
@@ -0,0 +1,28 @@
+// when we compare simple two numbers so its easy
+//to tell that what will be the result
+//FOR EXAMPLE
+
+//console.log(2>2)
+//console.log(2>=2)
+//console.log(2<=2)
+//console.log(2<2)
+//console.log(2==2)
+//console.log(2!=2)
+
+//when we compare two diffrent datatype
+
+//console.log("2">1) => true
+//console.log("02">1) => true
+// here 2 is converted into number
+
+//console.log(null>=0) => true 
+//console.log(null==0) => false
+//here for "==" null and 0 compare "equialy" (data type will also compare)
+//and for <, >, <=, >= its is converted into 0
+
+//console.log(undefined == 0) => false
+//console.log(undefined < 0) => false
+//console.log(undefined > 0) => false
+
+// ===
+//(it strictaly checks because it compare dattype thats why we mostly use this comparision)
