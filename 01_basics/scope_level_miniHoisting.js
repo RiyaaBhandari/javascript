05cc59fde1a40662c074b7e9a15c4e98aa85e74a
RiyaaBhandari <127602057+RiyaaBhandari@users.noreply.github.com>
2024-03-07 05:13:33 +0000

    functin scope level

---
+++ 01_basics/scope_level_miniHoisting.js
@@ -0,0 +1,41 @@
+//function one(){
+    //const username="riya"
+
+    //function two(){
+    //    const website="youtube"
+    //    console.log(username)
+  //  }
+    //console.log(website);
+//
+  //  two()
+//}
+
+//one()
+
+if(true){
+    const user="hitesh"
+    if(user==="hitesh"){
+        const website="youtube"
+        console.log(user+website);
+    }
+    //console.log(website)
+    console.log(user)
+}
+//console.log(username)
+
+//-------------------------------- intresting -----------------------------------------
+
+addone(6)
+
+function addone(num){
+  return num+2
+}
+
+addtwo(6)
+
+const addtwo = function(num){
+  return num+1
+}
+//this function gives error because we are storing this function in a variable 
+//and we cannot acsses variable before initialization
+
